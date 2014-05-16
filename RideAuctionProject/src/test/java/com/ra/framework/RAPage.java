4c5d57a9560cf4fa99cd2b54f2c19aa2680e02b7
hbijli <hbijli@hotmail.com>
2014-05-16 09:26:18 -0700

    updates RAPAge, WebDriverTest

--- RideAuctionProject/src/test/java/com/ra/framework/RAPage.java
+++ RideAuctionProject/src/test/java/com/ra/framework/RAPage.java
@@ -28,6 +28,9 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import static org.testng.Assert.*;
 
 public class RAPage {
 	
@@ -40,15 +43,97 @@ public class RAPage {
 		this.driver = driver;
 		
 	}
+		
+	public WebElement getElement(By elmLocator) {
+		
+		try {
+			return driver.findElement(elmLocator);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+			
+	}
 	
-	WebElement getElement(By elmLocator) {
+	public boolean isElementPresent(By elmLocator) {
 		
-		return driver.findElement(elmLocator);
-		
+		if (getElement(elmLocator) != null)
+			return true;
+		else
+			return false;
 	}
 	
 	
+	public void type (By elmLocator, String value) {
+		WebElement elm = getElement(elmLocator);	
+		assertNotNull(elm);
+		elm.sendKeys(value);
+	}
 	
+	public void verifyAndClick (By elmLocator) {
+		WebElement elm = getElement(elmLocator);
+		assertNotNull(elm);
+		elm.click();		
+	}
 	
+	public void selectDropDownByVisibleText(By elmLocator, String value) {
+		WebElement elm = getElement(elmLocator);
+		assertNotNull(elm);
+		
+		Select select = new Select (elm);
+		select.selectByVisibleText(value);
+		
+	}
+	
+	public void selectDropDownByValue (By elmLocator, String value) {
+		WebElement elm = getElement(elmLocator);
+		assertNotNull(elm);
+		
+		Select select = new Select (elm);
+		select.selectByValue(value);
+	}
+	
+	public void selectDropDownByIndex (By elmLocator, int index) {
+		WebElement elm = getElement(elmLocator);
+		assertNotNull(elm);
+		
+		Select select = new Select (elm);
+		select.selectByIndex(index);
+	}
+	
+	public void verifyElementSelected (By elmLocator) {
+		WebElement elm = getElement (elmLocator);
+		assertNotNull(elm);
+		assertTrue(elm.isSelected());
+	}
+	
+	public void verifyElementEnabled (By elmLocator) {
+		WebElement elm = getElement (elmLocator);
+		assertNotNull(elm);
+		assertTrue(elm.isEnabled());
+	}
+	
+	public void verifyElementDisplayed (By elmLocator) {
+		WebElement elm = getElement (elmLocator);
+		assertNotNull(elm);
+		assertTrue(elm.isDisplayed());
+	}
+	
+	public void verifyTextPresent (By elmLocator, String text) {
+		WebElement elm = getElement (elmLocator);
+		assertNotNull(elm);
+		String actualText = elm.getText();
+		assertEquals(actualText, text);
+	}
+	
+	void checkCheckBoxElement(By elmLocator) {
+		WebElement elm = getElement (elmLocator);
+		assertNotNull(elm);
+		
+		verifyAndClick(elmLocator);
+		assertTrue(elm.isSelected());
+		 
+	 }
 
 }
--- RideAuctionProject/src/test/java/com/ra/framework/WebDriverTest.java
+++ RideAuctionProject/src/test/java/com/ra/framework/WebDriverTest.java
@@ -1,21 +1,78 @@
-/*************************************************
+/****************************************************************************
  * WebDriverTest.java
  * Daddy class for all the tests.
+ * Here the setup methods that need to run before the test cases are defined.
  * 
- * 
- ************************************************/
-/************************************************
+ ***************************************************************************/
+/************************************************************
  * Methods:
+ * runBeforeEachMethod()
+ * runAfterEachMethod()
+ * takeScreenshotOnFailure()
  * 
- * 
- ***********************************************/
+ ***********************************************************/
 
 package com.ra.framework;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.apache.log4j.Logger;
+
+import static org.testng.Assert.*;
+
 
 public class WebDriverTest {
 	
 	protected WebDriver driver;
 
-}
+	Logger logger = Logger.getLogger(WebDriverTest.class);
+	
+	@BeforeMethod (alwaysRun=true)
+	public void runBeforeEachMethod() {
+		
+		logger.debug("Starting new test case.");	
+		driver = WebDriverManager.getWebDriver();
+	}
+	
+	@AfterMethod (alwaysRun=true)
+	public void takeScreenshotOnFailure(ITestResult result) {
+		
+		logger.debug("Screenshot on failure."+result.getName());
+		
+		if (!result.isSuccess()) {
+			Calendar calendar = Calendar.getInstance();
+			SimpleDateFormat formatter = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
+			String fileName = PropertyManager.getProperty("screen_shot_dir")+result.getName()+formatter.format(calendar.getTime()+".png");
+			
+			File screenshotToBeCopied = new File(fileName);
+			File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			try {
+				FileUtils.copyFile(screenshotFile,screenshotToBeCopied);
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			} // end catch
+		}//end if
+		
+	}//end method
+	
+	@AfterMethod (alwaysRun=true)
+	public void runAfterEachMethod() {
+		
+		logger.debug("Ending current test case.");
+		driver.close();
+		driver.quit();
+		
+	}
+}// end class
